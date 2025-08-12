48b075045cd500f1caa32e74948a7144f00b7da9
Henry Wilde <henrydavidwilde@gmail.com>
2025-08-12 17:13:37 +0100

    Implement composite node (need to finish tests)

--- src/certus/node.py
+++ src/certus/node.py
@@ -2,8 +2,11 @@
 
 import dataclasses
 import math
+import typing
 
 from . import utils
+
+NodeType = typing.Union["TokenNode", "CompositeNode"]
 
 
 @dataclasses.dataclass
@@ -20,7 +23,7 @@ class TokenNode:
 
     Attributes
     ----------
-    confidence : float, optional
+    confidence : float
         Confidence (probability) of the token.
     """
 
@@ -28,7 +31,7 @@ class TokenNode:
     logprob: float
 
     def __post_init__(self):
-        self._confidence = None
+        self._confidence: float | None = None
 
     @property
     def confidence(self) -> float:
@@ -37,3 +40,74 @@ class TokenNode:
             self._confidence = utils.clamp(math.exp(self.logprob), 0.0, 1.0)
 
         return self._confidence
+
+
+@dataclasses.dataclass
+class CompositeNode:
+    """
+    Data model for a node made up of other nodes.
+
+    Parameters
+    ----------
+    children : list of TokenNode or CompositeNode
+        Nodes contained within this composite.
+
+    Attributes
+    ----------
+    confidence : float
+        Confidence of the composite. Derived as the geometric mean of
+        the log-probabilities of all downstream token (leaf) nodes.
+    """
+
+    children: list[NodeType]
+
+    def __post_init__(self):
+        self._value: str | None = None
+        self._logprob: float | None = None
+        self._confidence: float | None = None
+        self._leaves: list[TokenNode] | None = None
+
+    @property
+    def value(self) -> str:
+        """Set or return the concatenation of the composite's values."""
+        if self._value is None:
+            self._value = " ".join(leaf.value for leaf in self.leaves)
+
+        return self._value
+
+    @property
+    def logprob(self) -> float:
+        """Set or return the sum of the log-probs of the composite."""
+        if self._logprob is None:
+            self._logprob = sum(leaf.logprob for leaf in self.leaves)
+
+        return self._logprob
+
+    @property
+    def confidence(self) -> float:
+        """Set or return the confidence of the composite."""
+        if self._confidence is None:
+            mean_logprob = self.logprob / len(self.leaves)
+            self._confidence = utils.clamp(math.exp(mean_logprob), 0.0, 1.0)
+
+        return self._confidence
+
+    @property
+    def leaves(self) -> list[TokenNode]:
+        """Return the leaf nodes downstream of this composite node."""
+        if self._leaves is None:
+            self._leaves = self._gather_leaves()
+
+        return self._leaves
+
+    def _gather_leaves(self) -> list[TokenNode]:
+        """Get the leaf nodes downstream of this composite node."""
+        leaves = []
+        for child in self.children:
+            if isinstance(child, TokenNode):
+                leaves.append(child)
+                continue
+
+            leaves.extend(child._gather_leaves())
+
+        return leaves
