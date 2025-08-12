41866c6aa9f36c0106ebf64a218e07bb8fd1b594
Henry Wilde <henrydavidwilde@gmail.com>
2025-08-12 17:09:06 +0100

    Write tests for most of composite node model

--- tests/test_node.py
+++ tests/test_node.py
@@ -8,10 +8,22 @@ import hypothesis.strategies as st
 
 from certus import node
 
-GIVEN_TOKEN_NODE_PARAMS = hyp.given(st.text(), st.floats(max_value=0))
+ST_LOGPROBS = st.floats(max_value=0)
+ST_TOKEN_NODES = st.builds(node.TokenNode, logprob=ST_LOGPROBS)
+
+ST_COMPOSITE_NODES = st.recursive(
+    ST_TOKEN_NODES,
+    lambda children: st.builds(
+        node.CompositeNode, children=st.lists(children, min_size=1, max_size=3)
+    ),
+    max_leaves=10,
+).filter(lambda n: isinstance(n, node.CompositeNode))
+
+ST_EMPTY_COMPOSITE_NODES = st.builds(node.CompositeNode, children=st.just([]))
+ST_LEAF_LISTS = st.lists(ST_TOKEN_NODES, min_size=1)
 
 
-@GIVEN_TOKEN_NODE_PARAMS
+@hyp.given(st.text(), ST_LOGPROBS)
 def test_token_node_init(value, logprob):
     """Check a node is instantiated as expected."""
     token = node.TokenNode(value, logprob)
@@ -21,7 +33,7 @@ def test_token_node_init(value, logprob):
     assert token._confidence is None
 
 
-@GIVEN_TOKEN_NODE_PARAMS
+@hyp.given(st.text(), ST_LOGPROBS)
 def test_token_node_confidence_one_time(value, logprob):
     """
     Check a node calculates its confidence only once.
@@ -34,9 +46,97 @@ def test_token_node_confidence_one_time(value, logprob):
     with mock.patch.object(node.utils, "clamp") as clamp:
         clamp.side_effect = lambda p, _, __: p
         token = node.TokenNode(value, logprob)
-        confidence = token.confidence
-        _ = token.confidence
+        c1 = token.confidence
+        c2 = token.confidence
 
-    assert confidence == math.exp(logprob)
+    assert c1 == c2 == math.exp(logprob)
 
-    clamp.assert_called_once_with(confidence, 0.0, 1.0)
+    clamp.assert_called_once_with(c1, 0.0, 1.0)
+
+
+@hyp.given(ST_COMPOSITE_NODES)
+def test_composite_node_init(composite):
+    """Check a node is instantiated as expected."""
+    children = composite.children
+    assert isinstance(children, list)
+    assert all(isinstance(child, (node.TokenNode, node.CompositeNode)) for child in children)
+
+    assert composite._value is None
+    assert composite._logprob is None
+    assert composite._confidence is None
+    assert composite._leaves is None
+
+
+@hyp.given(ST_EMPTY_COMPOSITE_NODES, ST_LEAF_LISTS)
+def test_composite_node_value_one_time(composite, leaves):
+    """
+    Check a node calculates its value only once.
+
+    We mock the leaf gatherer so we can ensure it is only called once,
+    passing a set of leaf nodes.
+    """
+    with mock.patch.object(composite, "_gather_leaves", return_value=leaves) as gather_leaves:
+        v1 = composite.value
+        v2 = composite.value
+
+    assert v1 == v2 == " ".join(leaf.value for leaf in leaves)
+
+    gather_leaves.assert_called_once_with()
+
+
+@hyp.given(ST_EMPTY_COMPOSITE_NODES, ST_LEAF_LISTS)
+def test_composite_node_logprob_one_time(composite, leaves):
+    """
+    Check a node calculates its log-probability only once.
+
+    We mock the leaf gatherer so we can ensure it is only called once,
+    passing a set of leaf nodes.
+    """
+    with mock.patch.object(composite, "_gather_leaves", return_value=leaves) as gather_leaves:
+        l1 = composite.logprob
+        l2 = composite.logprob
+
+    assert l1 <= 0
+    assert l1 == l2 == sum(leaf.logprob for leaf in leaves)
+
+    gather_leaves.assert_called_once_with()
+
+
+@hyp.given(ST_EMPTY_COMPOSITE_NODES, ST_LEAF_LISTS)
+def test_composite_node_confidence_one_time(composite, leaves):
+    """
+    Check a node calculates its confidence only once.
+
+    We mock the leaf gatherer so we can ensure it is only called once,
+    passing a set of leaf nodes. We also mock the clamp utility to pass
+    the probability through unchanged.
+    """
+    with (
+        mock.patch.object(composite, "_gather_leaves", return_value=leaves) as gather_leaves,
+        mock.patch.object(node.utils, "clamp", side_effect=lambda p, _, __: p) as clamp,
+    ):
+        c1 = composite.confidence
+        c2 = composite.confidence
+
+    assert 0 <= c1 <= 1
+    assert c1 == c2 == math.exp(sum(leaf.logprob for leaf in leaves) / len(leaves))
+
+    gather_leaves.assert_called_once_with()
+    clamp.assert_called_once_with(c1, 0.0, 1.0)
+
+
+@hyp.given(ST_EMPTY_COMPOSITE_NODES, ST_LEAF_LISTS)
+def test_composite_node_leaves_one_time(composite, leaves):
+    """
+    Check a node calculates its leaves only once.
+
+    We mock the leaf gatherer so we can ensure it is only called once,
+    passing a set of leaf nodes.
+    """
+    with mock.patch.object(composite, "_gather_leaves", return_value=leaves) as gather_leaves:
+        l1 = composite.leaves
+        l2 = composite.leaves
+
+    assert l1 == l2 == leaves
+
+    gather_leaves.assert_called_once_with()
