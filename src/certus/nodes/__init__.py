80cabab5ff7ecedbdfdd1063850d5a1675a5f6bb
Henry Wilde <henrydavidwilde@gmail.com>
2025-08-13 15:35:55 +0100

    Add structure node types to nodes access

--- src/certus/nodes/__init__.py
+++ src/certus/nodes/__init__.py
@@ -1,5 +1,6 @@
 """Subpackage for all node models."""
 
 from .core import Composite, Token
+from .struct import Array, Object
 
-__all__ = ["Composite", "Token"]
+__all__ = ["Array", "Composite", "Object", "Token"]
