ff15c0b8ceafb333dc409f49db0e5e290bdd1f01
Henry Wilde <henrydavidwilde@gmail.com>
2025-08-13 13:31:57 +0100

    Implement the object node

--- src/certus/nodes/struct.py
+++ src/certus/nodes/struct.py
@@ -31,3 +31,44 @@ class Array(Composite):
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(elements={self.elements!r})"
+
+
+@dataclasses.dataclass(kw_only=True)
+class Object(Composite):
+    """
+    Node representing a JSON object.
+
+    Parameters
+    ----------
+    fields : dict[str, NodeType]
+        Mapping from field names to child nodes.
+
+    Attributes
+    ----------
+    fields : dict[str, NodeType]
+        Stored mapping of field names to parsed nodes.
+    """
+
+    fields: dict[str, NodeType] = dataclasses.field(default_factory=dict)
+
+    def __post_init__(self) -> None:
+        self.children = list(self.fields.values())
+        super().__post_init__()
+
+    def __getitem__(self, key: str) -> NodeType:
+        return self.fields[key]
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(fields={self.fields!r})"
+
+    def keys(self) -> typing.KeysView[str]:
+        """Return the object's field keys."""
+        return self.fields.keys()
+
+    def items(self) -> typing.ItemsView[str, NodeType]:
+        """Return the object's (key, node) pairs."""
+        return self.fields.items()
+
+    def values(self) -> typing.ValuesView[NodeType]:
+        """Return the object's field values."""
+        return self.fields.values()
